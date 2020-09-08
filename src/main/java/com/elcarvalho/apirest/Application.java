8f832e6ade395ae284d4064341c86ccda3c18895
Eric Lessa <lessa.eric@gmail.com>
2020-09-08 17:47:27 -0300

    implementando dominios estado e cidade

--- src/main/java/com/elcarvalho/apirest/Application.java
+++ src/main/java/com/elcarvalho/apirest/Application.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.elcarvalho.apirest.domain.Categoria;
+import com.elcarvalho.apirest.domain.Cidade;
+import com.elcarvalho.apirest.domain.Estado;
 import com.elcarvalho.apirest.domain.Produto;
 import com.elcarvalho.apirest.repositories.CategoriaRepository;
+import com.elcarvalho.apirest.repositories.CidadeRepository;
+import com.elcarvalho.apirest.repositories.EstadoRepository;
 import com.elcarvalho.apirest.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class Application implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -42,6 +50,19 @@ public class Application implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 	}
 
