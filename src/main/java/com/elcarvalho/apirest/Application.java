63dd3750189c136ebf51802789475da8ab8909fe
Eric Lessa <lessa.eric@gmail.com>
2020-09-08 14:40:24 -0300

    criando operação de instanciação

--- src/main/java/com/elcarvalho/apirest/Application.java
+++ src/main/java/com/elcarvalho/apirest/Application.java
@@ -1,13 +1,32 @@
 package com.elcarvalho.apirest;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.elcarvalho.apirest.domain.Categoria;
+import com.elcarvalho.apirest.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
